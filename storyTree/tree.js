c0bee07f44142525d5aea9c19696ea9f9e6ae7e0
markableidinger <mark.ableidinger@gmail.com>
2014-08-26 17:27:45 -0700

    allows page to use local storage, doesn't allow user to creat blank nodes.

--- storyTree/tree.js
+++ storyTree/tree.js
@@ -1,118 +1,163 @@
+//content display fields
 var titleBox = $('#title');
 var contentBox = $('#content');
+var formBox = $('#form-box');
+var authorBox = $('#author');
+var endText = $('#end-text');
+var errorBox = $('#error-box')
+//nav buttons
 var buttonA = $('#a');
 var buttonB = $('#b');
 var previous = $('#back');
 var reset = $('#reset');
-var formBox = $('#form-box')
-var submit = $('#submit')
-var titleForm = $('#title-form')
-var contentForm = $('#content-form')
-var isEnding = $('#is-ending')
-var endText = $('#end-text')
+//form fields
+var titleForm = $('#title-form');
+var contentForm = $('#content-form');
+var isEnding = $('#is-ending');
+var authorForm = $('#author-form');
+var submit = $('#submit');
+var currentAddress = '';
 
-function Tree(){
-
-  this.currentAddress = '';
-
-  this.read = function(address){
-    this.currentAddress = address;
-    this.display()
+var nodes={};
+write = function(address, title, content, ending, author){
+  nodes[address] = {
+    'address' : address,
+    'title' : title,
+    'content' : content,
+    'ending' : ending,
+    'author' : author
   }
-
-  this.write = function(address, title, content, ending){
-    this[address] = new Node(address, title, content, ending);
-  }
-
-  this.traverse = function(choice){
-    if (this.hasOwnProperty(this.currentAddress+choice)){
-      this.currentAddress+=choice;
-      this.display()
-    }
-    else{
-      this.currentAddress+=choice;
-      this.newNodeForm()
-    }
-  }
-
-  this.find = function(title){
-    for(var x in this){
-      if (this[x].title==title){
-        return this[x];
-      }
-      else{
-        alert('no such node')
-      }
-    }
-  }
-
-  this.display = function(){
-    var node = this[this.currentAddress];
-    formBox.css('visibility','hidden')
-    titleBox.html(node.title)
-    contentBox.html(node.content)
-    if(node.ending){
-      buttonA.css('visibility', 'hidden')
-      buttonB.css('visibility', 'hidden')
-      endText.html('THE END')
-    }
-    else{
-      endText.html('')
-      buttonA.css('visibility', 'visible')
-      buttonB.css('visibility', 'visible')
-      if (this.hasOwnProperty(this.currentAddress+'a')){
-        buttonA.html(this[this.currentAddress+'a'].title)
-      }
-      else{
-        buttonA.html('Choose Your Own Adventure!')
-      }
-      if (this.hasOwnProperty(this.currentAddress+'b')){
-        buttonB.html(this[this.currentAddress+'b'].title)
-      }
-      else{
-        buttonB.html('Choose Your Own Adventure!')
-      }
-    }
-  }
-
-  this.newNodeForm = function(){
-    formBox.css('visibility', 'visible')
-    titleBox.html('There\'s nothing here yet!')
-    contentBox.html('Write the next part of the story')
-    buttonA.css('visibility', 'hidden')
-    buttonB.css('visibility', 'hidden')
-  }
-
+  store()
 }
 
-function Node(address, title, content, ending){
-  this.address = address
-  this.content = content
-  this.title = title
-  this.ending = ending
-  this.event = null
+function store(){
+  localStorage.setItem('nodes', JSON.stringify(nodes))
+}
+
+function retrieve(){
+  nodes = JSON.parse(localStorage.getItem('nodes'))
+}
+
+read = function(address){
+    currentAddress = address;
+    display();
+  }
+
+traverse = function(choice){
+  if (nodes.hasOwnProperty(currentAddress+choice)){
+    currentAddress+=choice;
+    display();
+  }
+  else{
+    currentAddress+=choice;
+    displayForm();
+  }
+}
+//currently unused
+find = function(title){
+  for(var x in nodes){
+    if (nodes[x].title==title){
+      return nodes[x];
+    }
+    else{
+      alert('no such node');
+    }
+  }
+}
+
+display = function(){
+  var node = nodes[currentAddress];
+  formBox.css('visibility','hidden');
+  authorBox.css('visibility', 'visible');
+  titleBox.html(node.title);
+  contentBox.html(node.content);
+  authorBox.html(node.author);
+  if(node.ending){
+    buttonA.css('visibility', 'hidden');
+    buttonB.css('visibility', 'hidden');
+    endText.html('THE END');
+  }
+  else{
+    endText.html('');
+    buttonA.css('visibility', 'visible');
+    buttonB.css('visibility', 'visible');
+    if (nodes.hasOwnProperty(currentAddress+'a')){
+      buttonA.html(nodes[currentAddress+'a'].title);
+    }
+    else{
+      buttonA.html('Choose Your Own Adventure!');
+    }
+    if (nodes.hasOwnProperty(currentAddress+'b')){
+      buttonB.html(nodes[currentAddress+'b'].title);
+    }
+    else{
+      buttonB.html('Choose Your Own Adventure!');
+    }
+  }
+}
+
+submitForm = function(){
+  errorString = ''
+  if (!titleForm.val()){
+    errorString += '<li>You must input a title</li>'
+  }
+  if (contentForm.val().length<=20){
+    errorString += '<li>Your content field must contain at least 20 characters</li>'
+  }
+  if (errorString){
+    errorBox.html(errorString)
+  }
+  else{
+    write(currentAddress, titleForm.val(), contentForm.val(), isEnding[0].checked, authorForm[0].value)
+    display()
+    clearForm()
+  }
+}
+
+displayForm = function(){
+    formBox.css('visibility', 'visible');
+    titleBox.html('There\'s nothing here yet!');
+    contentBox.html('Write the next part of the story');
+    buttonA.css('visibility', 'hidden');
+    buttonB.css('visibility', 'hidden');
+    authorBox.css('visibility', 'hidden');
+  }
+//currently not used
+function Node(address, title, content, ending, author){
+  this.address = address;
+  this.content = content;
+  this.title = title;
+  this.ending = ending;
+  this.event = null;
+  this.author = author || 'Anonymous';
 }
 
 function clearForm(){
-  titleForm[0].value=''
-  contentForm[0].value=''
-  isEnding[0].checked=false
+  titleForm[0].value='';
+  contentForm[0].value='';
+  authorForm[0].value='';
+  isEnding[0].checked=false;
 }
-
-var story = new Tree()
-story.write('', 'Root', 'This is the Root', false)
-story.write('a', 'Choice A', 'This is choice A', false)
-story.write('b', 'Choice B', 'This is choice B', false)
-story.write('aa', 'Choice AA', 'This is choice AA', false)
-story.write('ab', 'Choice AB', 'This is choice AB', true)
-story.write('ba', 'Choice BA', 'This is choice BA', true)
-story.write('bb', 'Choice BB', 'This is choice BB', true)
-
-story.display()
-buttonA.click(function(){story.traverse('a')})
-buttonB.click(function(){story.traverse('b')})
-previous.click(function(){story.read(story.currentAddress.slice(0, -1))})
-reset.click(function(){story.read('')})
-submit.click(function(){story.write(story.currentAddress, titleForm.val(), contentForm.val(), isEnding[0].checked)})
-submit.click(function(){story.display()})
-submit.click(function(){clearForm()})
+// initial writing of nodes
+if (localStorage.getItem('nodes')==null){
+  write('', 'Root', 'This is the Root', false, 'aksdjfkljsdh');
+  write('a', 'Choice A', 'This is choice A', false, '');
+  write('b', 'Choice B', 'This is choice B', false, '');
+  write('aa', 'Choice AA', 'This is choice AA', false, '');
+  write('ab', 'Choice AB', 'This is choice AB', true, '');
+  write('ba', 'Choice BA', 'This is choice BA', true, '');
+  write('bb', 'Choice BB', 'This is choice BB', true, '');
+}
+else{
+  retrieve()
+}
+//displays the node at address ''
+display()
+//event handling
+buttonA.click(function(){traverse('a')})
+buttonB.click(function(){traverse('b')})
+previous.click(function(){read(currentAddress.slice(0, -1))})
+reset.click(function(){read('')})
+submit.click(function(){submitForm()})
+// submit.click(function(){display()})
+// submit.click(function(){clearForm()})
