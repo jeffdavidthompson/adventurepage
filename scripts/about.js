3e66b217339f8141a35331d125c77458ecf7ebd8
Jeff <jeffdavidthompson@gmail.com>
2014-08-28 15:08:20 -0700

    restyle about page

--- scripts/about.js
+++ scripts/about.js
@@ -1,4 +1,4 @@
 $('.po').scroll(function(){
-var x = $('html,body').scrollTop();
-$(this).css('background-position','0% '+parseInt(-x/5)+'px');
+var x = $(this).scrollTop();
+$(this).css('background-position','0% '+parseInt(-x/10)+'px');
 });
